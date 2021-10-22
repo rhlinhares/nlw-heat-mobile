817cd4d95faba7e69570948a4337d0a2db6015ce
Raphael Hubener Linhares <raphael.linhares@gmail.com>
2021-10-22 19:21:46 -0300

    First commit - NLW Heat

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Home } from './src/screens/Home';
+import{ useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+import { AuthProvider } from './src/hooks/auth';
+
+
 
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <StatusBar
+      translucent 
+      style="light"/>
+      <Home/>
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+import { Button } from '../Button'
+import { COLORS } from '../../theme';
+
+import { styles } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+export function SignInBox(){
+  const { signIn, isSigningIn } = useAuth();
+
+  return (
+    <View style={styles.container}>
+
+      <Button 
+      title="ENTRAR COM GITHUB"
+      color={COLORS.BLACK_PRIMARY}
+      backgroundColor={COLORS.YELLOW}
+      icon="github"
+      onPress={signIn}
+      isLoading={isSigningIn}
+      />
+
+    </View>
+  );
+}
